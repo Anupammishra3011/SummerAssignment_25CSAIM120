3bd4a9faaa7df936956aeec5cf8635344ba554b2
Anupam Mishra <anupam.mishra2025@glbajajgroup.org>
2026-06-18 11:24:39 +0530

    DAY1 assignment is uploaded

--- Q1.c
+++ Q1.c
@@ -1,15 +1,18 @@
-//Question 1 -Write a program to calculate the sum of first n natural numbers
-//Answer-
-#include<stdio.h>
-int main()
-{
-    int i,n,sum=0;
-    printf("enter the number till you want the sum");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        sum+=i;
+//Question 1- Program to find sum of first n natural numbers
+// Answer-
+#include <stdio.h>
+
+int main() {
+    int i,n, sum = 0;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for( i = 1; i <= n; i++) {
+        sum += i;
     }
-    printf("Sum=%d",sum);
+
+    printf("Sum = %d", sum);
+
     return 0;
 }
